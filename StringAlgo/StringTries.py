72f91a01a6f6174b5a8fc444e347a1c1bc4f8cef
Mantosh Kumar <mantosh4u@gmail.com>
2023-02-08 01:25:52 +0530

    Create StringTries.py

---
+++ StringAlgo/StringTries.py
@@ -0,0 +1,30 @@
+# String matching often involves searching a large fixed body of text.
+# Make multiple queries on this text.
+# Preprocess the text to make the search efficient.
+# The goal is to come down from O(n+m) to O(m) where n = len(T) and m = len(P).
+# This essentially means that  we want to be independent of the text length and it
+# should just be length of pattern.
+
+# Trie record the words of text into special tree as part of preprocessing.
+
+# Suppose a text contains the following words.
+# {bear, bell, bid, bull, buy, sell, stop, stock}
+
+
+
+
+
+
+
+
+
+
+####################################################################################
+##############################________main_________#################################
+####################################################################################
+
+
+
+
+
+print("Completed Sucessfully")
