9a8ad147fbf8ef5c27afe4dc15cdb8a2bbd439ad
Mantosh Kumar <mantosh4u@gmail.com>
2023-02-08 18:13:23 +0530

    Create BasicNumberTheory.py

---
+++ NumberTheory/BasicNumberTheory.py
@@ -0,0 +1,14 @@
+# Every positive integer a is divisible by the trivial divisors 1 and a. The nontrivial
+# divisors of a are the factors of a. For example, the factors of 20 are 2, 4, 5, and 10.
+# Prime and composite numbers
+
+# An integer a > 1 whose only divisors are the trivial divisors 1 and a is a prime
+# number or, more simply, a prime. Primes have many special properties and play a
+# critical role in number theory. The first 20 primes, in order, are
+# 2 3 5 7 11 13 17 19 23 29 31 37 41 43 47 53 59 61 67 71
+
+# An integer a > 1 that is not prime is a composite number or, more simply, a 
+# composite. For example, 39 is composite because 3|39.
+
+
+
