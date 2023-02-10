45ae585556ec5a83f93f1809ef232c11bb7f6420
Mantosh Kumar <mantosh4u@gmail.com>
2023-02-10 11:03:14 +0530

    Update BasicNumberTheory.py

--- NumberTheory/BasicNumberTheory.py
+++ NumberTheory/BasicNumberTheory.py
@@ -27,3 +27,47 @@
 # 1 OR 0 = 1
 # 1 OR 1 = 1
 
+
+# Division
+#---------
+# Let Z be the set of integers.
+# Z = {.... -3, -2, -1, 0, 1, 2, 3 .....}
+# Let x and y are any two integers, that is x, y $ Z.
+# We say that x divides y, and write x|y, if there is q $ Z such that
+#       y = qx
+
+# Quotient and reminder
+#----------------------
+# Let x and y are any two integers, that is x, y $ Z.
+# It is possible to find integers q and r such that
+#   y = qx + r
+# where 0<= r <= x -1
+# The integer q is called quotient and r is called reminder.
+# we can write r = y mod x
+
+# If we replace y by y mod x, we say that y is reduced modulo x.
+# r' = (y mod x) mod x
+# What it signifiy?. I do not know.
+
+# Greatest common divisor
+#------------------------
+# The gcd of x, y $ X is positive integers denoted by gcd(x,y) such that
+#   gcd(x,y)|x and gcd(x,y)|y;
+#   if q|x and q|y, then q|gcd(x,y).
+
+# It is known that given any x,y $ Z there exist r,s $  such that
+#   rx + sy = gcd(x,y)
+
+
+
+
+
+# ####################################################################################
+# ##############################________main_________#################################
+# ####################################################################################
+
+
+
+
+print("Completed Sucessfully")
+
